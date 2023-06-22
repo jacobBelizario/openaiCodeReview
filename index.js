5065fa980214d2bfc6780d10d570edd35bad6892
jacobBelizario <jbelizario@plative.com>
2023-06-22 08:44:37 -0400

    should return parsed url from parameters

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const AI_API_KEY = core.getInput("openai_api_key", { required: true }) || "";
 const AI_MODEL = core.getInput("openai_model") || "gpt-3.5-turbo";
 const DIFF_FROM = core.getInput("diff_from") || "HEAD~1";
 const DIFF_TO = core.getInput("diff_to") || "HEAD";
+const current_sha = core.getInput("current_sha") || "none";
+const ghurl = core.getInput("ghurl") || "https://github.com";
 
 (async () => {
   try {
-    core.info(`Diff from: ${DIFF_FROM}`);
-    core.info(`Diff to: ${DIFF_TO}`);
+    core.info(`parsed url = https://github.com/${ghurl}/commit/${current_sha}`);
   } catch (error) {
     core.setFailed(error.message);
   }
