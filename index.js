a7f0845079bd5c533cc49244d99a4f5dbb2fc593
jacobBelizario <jbelizario@plative.com>
2023-06-30 07:38:57 -0400

    changed from html to markdown

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ const diff_file = core.getInput("diff_file" || "");
     const files = diff_code.split(" ");
     const tempdiff = diff_file.split("diff --git");
     const diffs = tempdiff.slice(1);
-    var output = `**ATTENTION:** ***This is a Plaito AI-generated code review, designed to surface possible concerns related to
+    var output = `**ATTENTION:** _This is a Plaito AI-generated code review, designed to surface possible concerns related to
       information security, code quality, and adherence to coding best practices. Developers and Development
       Managers/Architects are encouraged to utilize this tool to enhance their code review process. However,
-      please remember that this AI assistance does not alleviate the need for thorough human evaluation.***`;
+      please remember that this AI assistance does not alleviate the need for thorough human evaluation._`;
 
     const codeQueries = [];
 
