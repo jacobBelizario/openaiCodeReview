8ca4bcd2a1f864bd5e026f30a38cc76e58431af0
jacobBelizario <jbelizario@plative.com>
2023-07-17 08:23:03 -0400

    updated code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
 const axios = require("axios");
 const { LLMChain } = require("langchain/chains");
 const { ChatAnthropic } = require("langchain/chat_models/anthropic");
@@ -9,6 +8,7 @@ const ANTHROPIC_API_KEY = core.getInput("anthropic_key", { required: true });
 const ghurl = core.getInput("ghurl");
 const GITHUB_ACCESS_TOKEN = core.getInput("gh_token");
 const diff_code = core.getInput("diff_code" || "");
+const diff_branch = core.getInput("diff_branch" || "");
 const diff_file = core.getInput("diff_file" || "");
 (async () => {
   try {
@@ -20,6 +20,7 @@ const diff_file = core.getInput("diff_file" || "");
       modeltokens: 100000,
     });
     const files = diff_code.split(" ");
+    core.info(`diff file is ${diff_file}`);
     const tempdiff = diff_file.split("diff --git");
     const diffs = tempdiff.slice(1);
     var output = `**ATTENTION:** _This is a Plaito AI-generated code review, designed to surface possible concerns related to
@@ -35,32 +36,34 @@ const diff_file = core.getInput("diff_file" || "");
     }
 
     for (var codeQuery of codeQueries) {
-      const parsed_url = `https://github.com/${ghurl}/blob/main/${codeQuery.file}`;
+      const parsed_url = `https://github.com/${ghurl}/blob/${diff_branch}/${codeQuery.file}`;
+      const main_url = `https://github.com/${ghurl}/blob/main/${codeQuery.file}`;
       core.info(
         `codequery diff is: ${codeQuery.diff} file is: ${codeQuery.file}`
       );
-      core.info(parsed_url);
       const review_code = await getTextFromGitHub(parsed_url);
+      const base_code = await getTextFromGitHub(main_url);
+      core.info(`base code: ${base_code}`);
+      core.info(`parsed_url is  ${parsed_url}`);
 
       const template =
-        "Given this {code} Pretend you're a rigorous code reviewer who can analyze and comment on any code to prevent security violations, improve code quality, and enforce coding best practices.\n*CODE SUMMARY:*\nDescribe what type of code this is including the language and purpose.\n*CODE REVIEW:*\n*1. Observation: blah blah blah*\n        - Reasoning:\n        - Code Example:  \n        - Code Recommendation:  ";
+        "Given this {code} Pretend you're a rigorous code reviewer who can analyze and comment on any code to prevent security violations, improve code quality, and enforce coding best practices. Use ``` syntax for any code blocks. \n*CODE SUMMARY:*\nDescribe what type of code this is including the language and purpose.\n*CODE REVIEW:*\n*1. Observation: blah blah blah*\n        - Reasoning:\n        - Code Example:  \n        - Code Recommendation:  ";
       const prompt = new PromptTemplate({
         template: template,
         inputVariables: ["code"],
       });
-      core.info(review_code);
       const chain = new LLMChain({ llm: model, prompt: prompt });
       const chain_res = await chain.call({ code: review_code });
       //second chain
       const template1 =
-        "Below is a source code file followed by a DIFF showing what the developer has changed in the source code. Please describe the changes made by the developer, whether these changes improve code or make it worse. Explain your reasoning. \n\nFULL SOURCE CODE:{code} \n\nDIFF:{diff}";
+        "Below is a source code file followed by a DIFF showing what the developer has changed in the source code. Please describe the changes made by the developer, whether these changes improve code or make it worse. Explain your reasoning. Use ``` syntax for any code blocks. \n\nFULL SOURCE CODE:{code} \n\nDIFF:{diff}";
       const prompt2 = new PromptTemplate({
         template: template1,
         inputVariables: ["code", "diff"],
       });
       const chain2 = new LLMChain({ llm: model, prompt: prompt2 });
       const chain_res2 = await chain2.call({
-        code: review_code,
+        code: base_code,
         diff: codeQuery.diff,
       });
       // output += `SOURCE: ${parsed_url} \n${chain_res.text}\n\nANALYSIS OF CODE CHANGES:\n${chain_res2.text}\n\n${strTemplate}`;
@@ -80,7 +83,7 @@ const diff_file = core.getInput("diff_file" || "");
       .replace(/ {2,}/g, "")
       .replace(/\$/g, "\\$");
 
-    core.setOutput("openai_review", parsedOutput);
+    core.setOutput("ai_pr_review", parsedOutput);
   } catch (error) {
     core.setFailed(error.message);
   }
